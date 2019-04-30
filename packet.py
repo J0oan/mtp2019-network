c2b6bbe4d82ec09a3b1be04662734c436891e445
DuduLeitao <eduleitao@outlook.com>
2019-04-30 19:08:58 +0200

    Update of the packet class

--- packet.py
+++ packet.py
@@ -2,17 +2,33 @@
 
 class packet():
 
-    def __init__(self):
+    def init(self):
 
     def generateDiscovery ( origin ):
-        //Generar paquete Discovery con CRC y delvover array de bytes
+        #Generar paquete Discovery con CRC y delvover array de bytes
         return
 
-    def generateAckDiscovery ( rigin , destination ):
-        //Generar paquete ACK-Discovery con CRC y delvover array de bytes
+    def generateAckDiscovery ( origin , destination , flags ):
+        #Generar paquete ACK-Discovery con CRC y delvover array de bytes
         return
-    def genrateData
 
+    def generateData ( origin , destination , seqN , end , payload ):
+        #Generar paquete ACK-Discovery con CRC y delvover array de bytes
+        return
+
+    def generateAck (origin , destination , seqN ,  type ):
+        #Generar paquete ACK con CRC y delvover array de bytes
+        return
+
+    def generatetokenFrame ( origin , destination ):
+        #Generar paquete tokenFrame con CRC y delvover array de bytes
+        return
+
+    def generateAckTokenFrame ( origin , destination ):
+        #Generar paquete AckTokenFrame con CRC y delvover array de bytes
+        return
+    def endProtocol ( origin ):
+        return
 
     def decapsulatepacket(self):
-
+        
