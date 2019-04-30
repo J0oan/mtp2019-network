acffa4a8dbb8fb7e3df8e2b3875fa64d508ed848
clara <clabonros@gmail.com>
2019-04-30 18:13:24 +0200

    State Machine Definition

--- main.py
+++ main.py
@@ -23,9 +23,36 @@ while True:
 
         elif state is cte.BROADCAST_FLOODING:
             node.broacast_flooding()
+            if node.have_neighbors_without_file():
+                state = cte.SEND_PACKET
+            else:
+                state = cte.COMMUNICATION_OVER
 
         elif state is cte.BROADCAST_ACK:
             node.broadcast_ack()
+
+        elif state is cte.SEND_PACKET:
+            node.send_packet()
+            state = cte.PASS_TOKEN
+
+        elif state is cte.PASS_TOKEN:
+            node.pass_token()
+            state = cte.COMMUNICATION_OVER
+
+        elif state is cte.COMMUNICATION_OVER:
+            if node.any_neighbor_without_token():
+                # Consider neighbors with data and without token
+                state = cte.PASS_TOKEN
+
+            else:
+                if node.any_active_predecessor():
+                    node.return_token()
+                    stare = cte.COMMUNICATION_OVER
+                else:
+                    state = cte.END
+
+
+
 
 
 
--- node.py
+++ node.py
@@ -4,11 +4,17 @@ class Node(object):
         # Initialize node
         # Tx channel
         # Initialize radios
+    def send_data(self):
+        return NotImplementedError
 
-    def read_packet(self):
+    def read_data(self):
         return NotImplementedError
 
     def send_packet(self):
+        """
+        Uses send_data() to tx data
+        :return:
+        """
         return NotImplementedError
 
     def broadcast_flooding(self):
@@ -17,3 +23,21 @@ class Node(object):
     def broadcast_ack(self):
         return NotImplementedError
 
+    def has_neighbors(self):
+        """
+        True if it has neighbors, False otherwise
+        :return: Bool
+        """
+        return NotImplementedError
+
+    def have_neighbors_without_file(self):
+        return NotImplementedError
+
+    def any_neighbor_without_token(self):
+        return NotImplementedError
+
+    def any_active_predecessor(self):
+        return NotImplementedError
+
+    def return_token(self):
+        return NotImplementedError
